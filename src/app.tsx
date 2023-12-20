447b4433739e6dcc9096f62ca69482e28f1d13d6
Nazan <nazhard@proton.me>
2023-12-20 10:12:47 +0700

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,75 @@
+export function App() {
+  return (
+    <>
+      <h1>Hello, my name is Nazan (aka Nazhard)</h1>
+      <p>
+        I'm a junior full-stack web developer, junior high school student, and weeb.
+      </p>
+
+      <p>
+        I'm a coder who programs as a hobby and hopes to get a job as a full-stack developer in the future.
+        I'm not really interested in working,
+        I love programming,
+        maybe one day I'll just freelance.
+      </p>
+
+      <hr />
+
+      <h2> Somethings... \_(ツ)_/ </h2>
+      <p>
+        Most of my projects are on GitHub,
+        I don't have enough storage on my phone to store all the node_modules and files like index.html
+      </p>
+
+      <p>
+        Wish I could buy a new device,
+        I have a serious project at the moment,
+        and I need a tiger-powered machine.
+      </p>
+
+      <p>
+        By the way,
+        currently my projects are all private haha,
+        I'm just learning,
+        so a bit embarrassed, {"><"}
+      </p>
+
+      <hr />
+
+      <h2> Weeb.. </h2>
+      <p>
+        I have a wife,
+        she's a bassist in a band.
+        Her name is Nijika Ijichi.
+        Actually my wife is not just her hehe OwO
+      </p>
+
+      <hr />
+
+      <h2> Let's be friends with me :) </h2>
+      <ul>
+        <li>
+          <a href="https://github.com/nazhard">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://x.com/NazLightning">
+            X (formerly twitter)
+          </a>
+        </li>
+      </ul>
+
+      <hr />
+
+      <footer>
+        <p>
+          Thank you for reading,
+          I hope you can become a successful person.
+        </p>
+
+        <p>This is my prayer, for you.</p>
+      </footer>
+    </>
+  )
+}
